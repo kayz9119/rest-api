69fd6d990f72702ebca079e78784c15b7c097088
Sanalicyber <omerfaruk.h@outlook.com>
2022-04-27 14:48:07 +0300

    Launcher

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http')
 var express = require('express')
 var bodyPars = require('body-parser')
 
-var version = { "version": "1.0.2" }
+var version = { "version": "1.1.0" }
 var gameLink = {"link" : "https://www.mediafire.com/file/cnuolup7pyy83du/WarOfSea.zip/file"}
 
 var app = express();
