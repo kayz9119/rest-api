f9935db2ada1a62ddc96632384b6f1a63f0c68c4
Ömer Faruk Hilal <omerfaruk.h@outlook.com>
2022-07-09 00:26:13 +0300

    2.0.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http')
 var express = require('express')
 var bodyPars = require('body-parser')
 
-var version = { "version": "1.2.8" }
+var version = { "version": "2.0.0" }
 var gameLink = {"link" : "https://www.mediafire.com/file/esxcmuyx43k6ejr/WarOfSea.zip/file"}
 
 var app = express();
